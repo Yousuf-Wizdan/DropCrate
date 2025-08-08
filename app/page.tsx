3cdacde23ecae265c83b2335e5a4d6c34fd704fb
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-08-08 21:11:52 +0530

    Create SignInForm Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import SignInForm from "@/components/SignInForm";
 import SignUpForm from "@/components/SignUpForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <SignUpForm />
+      {/* <SignUpForm /> */}
+      <SignInForm />
     </div>
   );
 }
